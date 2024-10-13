3c9bc54b68b8788819a61656bf1d31bd5e917ebb
Takahiro Nakayama <takxxxaaa@gmail.com>
2024-10-13 17:57:45 +0900

    Add Terraform plan

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -21,10 +21,10 @@ terraform {
 # Provider
 # ---------------------------------------------
 provider "aws" {
-  region  = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
 provider "aws" {
-  alias   = "virginia"
-  region  = "us-east-1"
+  alias  = "virginia"
+  region = "us-east-1"
 }
